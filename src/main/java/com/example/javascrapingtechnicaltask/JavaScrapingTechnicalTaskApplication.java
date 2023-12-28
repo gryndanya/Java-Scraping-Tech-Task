289f7bbd1dd559994af4ed350a142ecb8ba8a9f8
Danyl Gryn <gryndanya@gmail.com>
2023-12-28 13:24:52 +0200

    Base

--- src/main/java/com/example/javascrapingtechnicaltask/JavaScrapingTechnicalTaskApplication.java
+++ src/main/java/com/example/javascrapingtechnicaltask/JavaScrapingTechnicalTaskApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaScrapingTechnicalTaskApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaScrapingTechnicalTaskApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JavaScrapingTechnicalTaskApplication.class, args);
+    }
 
 }
