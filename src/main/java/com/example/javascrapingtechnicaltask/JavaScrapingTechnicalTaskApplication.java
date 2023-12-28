2df8114251f3b6c96b4ab3a0aa5516ea8af22228
Danyl Gryn <gryndanya@gmail.com>
2023-12-28 13:24:14 +0200

    Initial commit

---
+++ src/main/java/com/example/javascrapingtechnicaltask/JavaScrapingTechnicalTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.javascrapingtechnicaltask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaScrapingTechnicalTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaScrapingTechnicalTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javascrapingtechnicaltask/JavaScrapingTechnicalTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javascrapingtechnicaltask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaScrapingTechnicalTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
